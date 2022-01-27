19f4f8b135d24a5bf578c81baaa6bb4903828225
antuanjoseff <toni.hernandez@udg.edu>
2022-01-27 17:56:41 +0100

    Work in progress. Grey scale

--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -232,6 +232,10 @@ button.fa-thin-button.active, button.fa-thin-button-menu.active {
 
 .toc-layers ul{
   list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+  padding:0 20px;
 }
 
 .toc-layers ul li{
@@ -239,11 +243,22 @@ button.fa-thin-button.active, button.fa-thin-button-menu.active {
 }
 
 .li-item img{
-  display: block
+  display: block;
+  margin:auto;
+}
+
+.li-item:hover img{
+  filter: grayscale(100%);
 }
 
 .li-item{
+  text-align: center;
   font-size:0.7rem;
   text-transform: capitalize;
+  width:70px;
+  margin:0 10px 10px 0px;
+  border-radius:10px;
+  border:1px solid black;
+  padding:5px;
 }
 </style>
