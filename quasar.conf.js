237a6ad3ef5fd604f7364aba0813b0d01871f099
antuanjoseff <toni.hernandez@udg.edu>
2022-06-04 16:38:57 +0200

    Public path

--- quasar.conf.js
+++ quasar.conf.js
@@ -49,7 +49,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      publicPath: 'https://sigserver4.udg.edu/mos/spa/',
+      publicPath: process.env.NODE_ENV === 'development' ? '/' : 'https://sigserver4.udg.edu/mos/spa/',
       // publicPath: '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
