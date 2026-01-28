a4692538410a10023707f72f4296fabd06d111ed
Enric Pou <enricpou@gmail.com>
2026-01-28 16:55:35 +0100

    Remove unused info/help buttons

--- src/components/MainLeftDrawer.vue
+++ src/components/MainLeftDrawer.vue
@@ -31,29 +31,7 @@
         <q-separator color="grey-4" />
         <ShareButton />
         <q-separator inset color="grey-4" />
-        <q-btn stack icon="fa fat fa-circle-info" />
-        <q-separator inset color="grey-4" />
-        <q-btn stack icon="fa fat fa-square-question" @click="showHelp = true" />
-        <q-separator inset color="grey-4" />
         <LanguageSwitcher />
-
-        <q-dialog v-model="showHelp">
-          <q-card>
-            <q-card-section>
-              <div class="text-h6">Alert</div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas
-              eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus
-              minima, porro labore.
-            </q-card-section>
-
-            <q-card-actions align="right">
-              <q-btn flat label="OK" color="primary" v-close-popup />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
 
       </q-btn-group>
     </q-toolbar>
@@ -79,11 +57,9 @@ export default {
   setup() {
 
     const drawerVisible = ref(true)
-    const showHelp = ref(false)
 
     return {
       drawerVisible,
-      showHelp
     }
   }
 }
