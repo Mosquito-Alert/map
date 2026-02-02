0cd8d087ab0327d6873f9a90768c461d9c2db1dc
Enric Pou <enricpou@gmail.com>
2026-02-02 17:21:29 +0100

    Convert sampling effort to WMS

--- src/components/SamplingEffortVectorLayer.vue
+++ src/components/SamplingEffortVectorLayer.vue
@@ -1,20 +1,18 @@
 <template>
-  <ol-vector-layer :visible="visible">
-    <ol-source-vector :url="computedUrl" :format="geoJson">
-      <ol-style :overrideStyleFunction="featureStyle"></ol-style>
-    </ol-source-vector>
-  </ol-vector-layer>
+  <ol-tile-layer :visible="visible">
+    <ol-source-tile-wms ref="sourceRef" :url="mapserver.getUri({ url: 'wms' })" layers="mosquitoalert:sampling_effort"
+      serverType="geoserver" />
+  </ol-tile-layer>
 </template>
 
 
 <script setup lang="ts">
 
-import type { Feature } from 'ol';
-import GeoJSON from 'ol/format/GeoJSON'
-import { Fill, Stroke, Style } from 'ol/style';
-import { computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 import { mapserver } from 'boot/axios'
+
+const sourceRef = ref()
 
 const props = defineProps<{
   visible: boolean,
@@ -22,47 +20,20 @@ const props = defineProps<{
   toDate: Date | undefined,
 }>()
 
-const geoJson = new GeoJSON();
-
-const computedUrl = computed(() => {
-  const viewParams = {
+const viewParams = computed(() => {
+  return {
     fromDate: props.fromDate ? props.fromDate.toISOString().split('T')[0] : undefined,
     toDate: props.toDate ? props.toDate.toISOString().split('T')[0] : undefined,
   }
-
-  const params = {
-    service: 'wfs',
-    version: '2.0.0',
-    request: 'GetFeature',
-    typeNames: 'mosquitoalert:sampling_effort',
-    outputFormat: 'application/json',
-    viewparams: Object.entries(viewParams).filter(([, value]) => value !== undefined).map(([key, value]) => `${key}:${value}`).join(';'),
-  }
-
-  return mapserver.getUri({ url: 'wfs', params })
 })
 
-const featureStyle = (feature: Feature) => {
-  const count = feature.get('count') || 0
-  let fillColor = 'rgba(255, 255, 178, 0.4)'
-
-  if (count > 1000) {
-    fillColor = 'rgba(189, 0, 38, 0.4)'
-  } else if (count > 100) {
-    fillColor = 'rgba(240, 59, 32, 0.4)'
-  } else if (count > 10) {
-    fillColor = 'rgba(253, 141, 60, 0.4)'
+watch(() => [props.fromDate, props.toDate], () => {
+  if (sourceRef.value === undefined) {
+    return
   }
-
-  return new Style({
-    fill: new Fill({
-      color: fillColor,
-    }),
-    stroke: new Stroke({
-      color: 'rgba(30, 30, 30, 0.4)',
-      width: 0.1,
-    }),
+  sourceRef.value.source.updateParams({
+    'viewparams': Object.entries(viewParams.value).filter(([, value]) => value !== undefined).map(([key, value]) => `${key}:${value}`).join(';'),
   })
-}
+}, { immediate: true })
 
 </script>
