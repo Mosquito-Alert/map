928259b31452ca19505cadc677872e9916e6680d
guillesanz21 <guillermo.sanz@ceab.csic.es>
2026-06-08 16:25:24 +0200

    Bigger lines and points for the drawing tool.

--- src/components/Drawer/AnalizeDrawer/DrawTool.ts
+++ src/components/Drawer/AnalizeDrawer/DrawTool.ts
@@ -1,6 +1,9 @@
 import MapboxDraw from '@mapbox/mapbox-gl-draw'
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 
+const drawActiveColor = '#fbb03b'
+const drawInactiveColor = '#3bb2d0'
+
 const styles = [
   {
     id: 'gl-draw-polygon-fill-inactive',
@@ -11,19 +14,27 @@ const styles = [
       ['==', '$type', 'Polygon'],
       ['!=', 'mode', 'static'],
     ],
-    paint: { 'fill-color': '#3bb2d0', 'fill-outline-color': '#3bb2d0', 'fill-opacity': 0.1 },
+    paint: {
+      'fill-color': drawInactiveColor,
+      'fill-outline-color': drawInactiveColor,
+      'fill-opacity': 0.1,
+    },
   },
   {
     id: 'gl-draw-polygon-fill-active',
     type: 'fill',
     filter: ['all', ['==', 'active', 'true'], ['==', '$type', 'Polygon']],
-    paint: { 'fill-color': '#fbb03b', 'fill-outline-color': '#fbb03b', 'fill-opacity': 0.1 },
+    paint: {
+      'fill-color': drawActiveColor,
+      'fill-outline-color': drawActiveColor,
+      'fill-opacity': 0.1,
+    },
   },
   {
     id: 'gl-draw-polygon-midpoint',
     type: 'circle',
     filter: ['all', ['==', '$type', 'Point'], ['==', 'meta', 'midpoint']],
-    paint: { 'circle-radius': 3, 'circle-color': '#fbb03b' },
+    paint: { 'circle-radius': 5, 'circle-color': drawActiveColor },
   },
   {
     id: 'gl-draw-polygon-stroke-inactive',
@@ -35,14 +46,14 @@ const styles = [
       ['!=', 'mode', 'static'],
     ],
     layout: { 'line-cap': 'round', 'line-join': 'round' },
-    paint: { 'line-color': '#3bb2d0', 'line-width': 2 },
+    paint: { 'line-color': drawInactiveColor, 'line-width': 4 },
   },
   {
     id: 'gl-draw-polygon-stroke-active',
     type: 'line',
     filter: ['all', ['==', 'active', 'true'], ['==', '$type', 'Polygon']],
     layout: { 'line-cap': 'round', 'line-join': 'round' },
-    paint: { 'line-color': '#fbb03b', 'line-dasharray': [0.2, 2], 'line-width': 2 },
+    paint: { 'line-color': drawActiveColor, 'line-dasharray': [0.2, 2], 'line-width': 4 },
   },
   {
     id: 'gl-draw-line-inactive',
@@ -54,26 +65,26 @@ const styles = [
       ['!=', 'mode', 'static'],
     ],
     layout: { 'line-cap': 'round', 'line-join': 'round' },
-    paint: { 'line-color': '#3bb2d0', 'line-width': 2 },
+    paint: { 'line-color': drawInactiveColor, 'line-width': 4 },
   },
   {
     id: 'gl-draw-line-active',
     type: 'line',
     filter: ['all', ['==', '$type', 'LineString'], ['==', 'active', 'true']],
     layout: { 'line-cap': 'round', 'line-join': 'round' },
-    paint: { 'line-color': '#fbb03b', 'line-dasharray': [0.2, 2], 'line-width': 2 },
+    paint: { 'line-color': drawActiveColor, 'line-dasharray': [0.2, 2], 'line-width': 4 },
   },
   {
     id: 'gl-draw-polygon-and-line-vertex-stroke-inactive',
     type: 'circle',
     filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
-    paint: { 'circle-radius': 5, 'circle-color': '#fff' },
+    paint: { 'circle-radius': 7, 'circle-color': '#fff' },
   },
   {
     id: 'gl-draw-polygon-and-line-vertex-inactive',
     type: 'circle',
     filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
-    paint: { 'circle-radius': 3, 'circle-color': '#fbb03b' },
+    paint: { 'circle-radius': 5, 'circle-color': drawActiveColor },
   },
   {
     id: 'gl-draw-point-point-stroke-inactive',
@@ -85,7 +96,7 @@ const styles = [
       ['==', 'meta', 'feature'],
       ['!=', 'mode', 'static'],
     ],
-    paint: { 'circle-radius': 5, 'circle-opacity': 1, 'circle-color': '#fff' },
+    paint: { 'circle-radius': 7, 'circle-opacity': 1, 'circle-color': '#fff' },
   },
   {
     id: 'gl-draw-point-inactive',
@@ -97,19 +108,19 @@ const styles = [
       ['==', 'meta', 'feature'],
       ['!=', 'mode', 'static'],
     ],
-    paint: { 'circle-radius': 3, 'circle-color': '#3bb2d0' },
+    paint: { 'circle-radius': 5, 'circle-color': drawInactiveColor },
   },
   {
     id: 'gl-draw-point-stroke-active',
     type: 'circle',
     filter: ['all', ['==', '$type', 'Point'], ['==', 'active', 'true'], ['!=', 'meta', 'midpoint']],
-    paint: { 'circle-radius': 7, 'circle-color': '#fff' },
+    paint: { 'circle-radius': 9, 'circle-color': '#fff' },
   },
   {
     id: 'gl-draw-point-active',
     type: 'circle',
     filter: ['all', ['==', '$type', 'Point'], ['!=', 'meta', 'midpoint'], ['==', 'active', 'true']],
-    paint: { 'circle-radius': 5, 'circle-color': '#fbb03b' },
+    paint: { 'circle-radius': 7, 'circle-color': drawActiveColor },
   },
   {
     id: 'gl-draw-polygon-fill-static',
@@ -122,20 +133,20 @@ const styles = [
     type: 'line',
     filter: ['all', ['==', 'mode', 'static'], ['==', '$type', 'Polygon']],
     layout: { 'line-cap': 'round', 'line-join': 'round' },
-    paint: { 'line-color': '#404040', 'line-width': 2 },
+    paint: { 'line-color': '#404040', 'line-width': 4 },
   },
   {
     id: 'gl-draw-line-static',
     type: 'line',
     filter: ['all', ['==', 'mode', 'static'], ['==', '$type', 'LineString']],
     layout: { 'line-cap': 'round', 'line-join': 'round' },
-    paint: { 'line-color': '#404040', 'line-width': 2 },
+    paint: { 'line-color': '#404040', 'line-width': 4 },
   },
   {
     id: 'gl-draw-point-static',
     type: 'circle',
     filter: ['all', ['==', 'mode', 'static'], ['==', '$type', 'Point']],
-    paint: { 'circle-radius': 5, 'circle-color': '#404040' },
+    paint: { 'circle-radius': 7, 'circle-color': '#404040' },
   },
 ]
 
@@ -177,5 +188,5 @@ export const updateDrawArea = (e: any) => {
     }
   }
 
-  console.log(e)
+  console.log(e) // DELETE:
 }
